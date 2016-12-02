e9ad3de98e99e01ab6fd24cf04a58d8e88cd3a9f
KiKyeong "K" Lim <klimdev@gmail.com>
2016-12-02 13:40:38 -0800

    d1,d2

---
+++ D1-2.py
@@ -0,0 +1,41 @@
+
+
+inputString = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3"
+#inputString = "R5, L5, R5, R3"
+
+#R +1, L -1
+totalStep = [0, 0, 0, 0]  # N R S L
+currentDirection = 0
+
+inputString = inputString.replace(' ', '')
+#print(inputString)
+commands = inputString.split(',')
+
+cityMap = {'0,0'}
+find = False
+
+for command in commands:
+    command = command.replace(',', '')
+    if command[0] == 'L':
+        currentDirection = (4 + currentDirection - 1) % 4
+        command = command.strip('L')
+    else:
+        currentDirection = (currentDirection + 1) % 4
+        command = command.strip('R')
+
+    step = 0
+    steps = int(command)
+    while step < steps:
+        step += 1
+        totalStep[currentDirection] += 1
+        coord = str(totalStep[0] - totalStep[2]) + ',' +  str(totalStep[1] - totalStep[3])
+        if coord in cityMap:
+            find = True
+            break
+        else:
+            cityMap.add(coord)
+
+    if find:
+        break
+
+print(abs(totalStep[0] - totalStep[2]) + abs(totalStep[1] - totalStep[3]))
